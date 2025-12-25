ee7fd87e060498a990665f88791b9b8288c6ccc2
hhtomaswt11 <tmelo949@gmail.com>
2025-12-25 15:42:20 +0000

    a

---
+++ Jogos/jogo28.pl
@@ -0,0 +1,24 @@
+:- dynamic pergunta/6.
+:- multifile pergunta/6.
+:- discontiguous pergunta/6.
+
+pergunta(1, 1, 500, "Qual a capital de Portugal?", ["Porto","Lisboa","Coimbra","Faro"], 'B').
+pergunta(2, 2, 750, "Quanto é 12 ÷ 3?", ["3","4","5","6"], 'B').
+pergunta(3, 3, 1000, "Qual é a capital da Itália?", ["Roma","Milão","Nápoles","Turim"], 'A').
+pergunta(4, 4, 1500, "Como se chama o processo de as plantas produzirem alimento?", ["Respiração","Digestão","Fotossíntese","Fermentação"], 'C').
+pergunta(5, 5, 3000, "Qual destes planetas é o mais próximo do Sol?", ["Vénus","Mercúrio","Terra","Marte"], 'B').
+pergunta(6, 6, 5000, "Quem escreveu 'Os Miseráveis'?", ["Balzac","Zola","Victor Hugo","Flaubert"], 'C').
+pergunta(7, 7, 7500, "Que planeta tem um grande anel à sua volta?", ["Júpiter","Saturno","Urano","Neptuno"], 'B').
+pergunta(8, 8, 10000, "Qual é o maior país do mundo em área?", ["China","EUA","Rússia","Canadá"], 'C').
+pergunta(9, 9, 15000, "Em que unidade se mede a força?", ["Watt","Newton","Joule","Pascal"], 'B').
+pergunta(10, 10, 20000, "Quem pintou 'A Persistência da Memória'?", ["Dali","Picasso","Matisse","Kandinsky"], 'A').
+pergunta(11, 11, 30000, "Qual é a capital da Suíça?", ["Genebra","Zurique","Berna","Basileia"], 'C').
+pergunta(12, 12, 45000, "Quantos dias tem um ano bissexto?", ["364","365","366","367"], 'C').
+pergunta(13, 13, 60000, "Qual é o metal mais leve?", ["Magnésio","Alumínio","Lítio","Titânio"], 'C').
+pergunta(14, 14, 80000, "Quem formulou a lei da gravitação universal?", ["Kepler","Newton","Einstein","Maxwell"], 'B').
+pergunta(15, 15, 100000, "Qual destes países tem a maior população?", ["Rússia","Brasil","Japão","Nigéria"], 'D').
+pergunta(16, 16, 150000, "Que partícula é responsável pela eletricidade?", ["Protão","Neutrão","Electrão","Fóton"], 'C').
+pergunta(17, 17, 250000, "Qual é a capital do Canadá?", ["Toronto","Vancouver","Ottawa","Montreal"], 'C').
+pergunta(18, 18, 500000, "Quem descobriu a lei da expansão do Universo?", ["Einstein","Hubble","Bohr","Dirac"], 'B').
+pergunta(19, 19, 750000, "Qual destas formações é uma galáxia?", ["Órion","Andrómeda","Sirius","Vénus"], 'B').
+pergunta(20, 20, 1000000, "Qual destes países é atravessado pelo rio Nilo?", ["Nigéria","Etiópia","Quénia","Egito"], 'D').
